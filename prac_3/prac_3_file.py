5ea01b5db3ca73be6273ba031d2b068a11935efd
nikolaj <strashevskiynn@g,ail.com>
2020-11-25 18:28:03 +0300

    file

--- prac_3/prac_3_file.py
+++ prac_3/prac_3_file.py
@@ -2,12 +2,12 @@ FILENAME = 'test.txt'
 
 
 def create_file(filename):
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'a', encoding='utf-8') as f:
         while True:
-            input_str = input('введите строку')
+            input_str = input('введите строку:')
             f.write(input_str)
             f.write('\n')
-
+            f.flush()  # моментальная запись на диск, без кэша
 
 
 

b30d7e4ff58652227d16e553dd6732b1093911a0
nikolaj <strashevskiynn@g,ail.com>
2020-12-03 15:28:13 +0300

    books.regex_args_pars

--- prac_4/books.py
+++ prac_4/books.py
@@ -1,17 +1,27 @@
 import re
 import json
+import argparse
 
 books_file = '/Users/nikolaj/PycharmProjects/py_110_for_git/prac_4/books.txt'
 
 
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', help='file name')
+
+    return parser
+
+
 def books():
+    parser = create_parser()
+    args = parser.parse_args()
     a = []
     pattern_books = re.compile(
         r'#{4}\s+(?P<position>\d{1,2})\.\s+\[(?P<book>.*?)\]\((?P<book_url>.*?)\)(?:\s+)?by(?:\s+)?(?P<autor>.*?)('
         r'?:\s+)?\((?P<recommended>.*?)\)(?:\s+)?!\[\]\((?P<cover_url>.*?)\)(?:\s+)?\"(?P<description>.*?)\"',
         re.DOTALL)
 
-    with open(books_file) as f:
+    with open(args.filename) as f:
         a = sorted(pattern_books.findall(f.read()), key=lambda i: int(i[0]), reverse=False)
         with open('books.json', 'a') as f:
             json.dump(a, f, indent=4)
